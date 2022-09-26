f672d7f6c8018f73b62f4032359e560e990606de
Mariappan <97144165+Sivarajavel@users.noreply.github.com>
2022-09-26 05:50:11 +0100

    Update FileHandle.java

--- FileHandle.java
+++ FileHandle.java
@@ -2,45 +2,69 @@ public class FileHandle
 {
 	private static int fileLineCount=1;
 	
-	private String displayFileContant()throws java.io.FileNotFoundException,java.io.IOException
+	private String displayFileContant()
 	{
-		java.io.FileReader file=new java.io.FileReader("E:/JDK 11/bin/FileHandle.java");
-		short i=0;
 		String fileContant=new String();
-		while( (i=(short)file.read()) != -1 )
+		try 
 		{
-			if(i==10)
-				fileLineCount++;
-			fileContant+=String.valueOf((char)i);
+			java.io.FileReader file=new java.io.FileReader("E:/JDK 11/bin/FileHandle.java");
+		
+			short i=0;
+		
+			while( (i=(short)file.read()) != -1 )
+			{
+				if(i==10)
+					fileLineCount++;
+				fileContant+=String.valueOf((char)i);
+			}
+		
+			file.close();
+		}catch(java.io.FileNotFoundException e)
+		{
+			System.out.println("File not Found Exception");
 		}
-		file.close();
+		catch(java.io.IOException e)
+		{
+			System.out.println("Enter the valid input");
+		}
 		return fileContant;
 	}
 	
-	private String getString()throws java.io.IOException
+	private String getString()
 	{
-		return ((new java.io.BufferedReader(new java.io.InputStreamReader(System.in))).readLine());
+		String retrunValue=new String();
+		try {
+			returnValue = ((new java.io.BufferedReader(new java.io.InputStreamReader(System.in))).readLine());
+		}catch(java.io.IOException e){}
+		return returnValue;
 	}
 	
-	private void changeFileContant(String fileContant)throws java.io.IOException,java.io.FileNotFoundException
+	private void changeFileContant(String fileContant)
 	{
-		FileHandle fileModifyObj=new FileHandle(); 
-		System.out.println("\nEnter the text...:");
-		String text=fileModifyObj.getString();
+		try 
+		{
+			FileHandle fileModifyObj=new FileHandle(); 
+			System.out.println("\nEnter the text...:");
+			String text=fileModifyObj.getString();
 		
-		System.out.println("Update the text...:");
-		String changeText=fileModifyObj.getString();
-		fileContant=fileContant.replace(text,changeText);
-		System.out.println(fileContant);
+			System.out.println("Update the text...:");
+			String changeText=fileModifyObj.getString();
+			fileContant=fileContant.replace(text,changeText);
+			System.out.println(fileContant);
 		
-		System.out.println("NUMBER OF LINE IN A FILE...:  < "+fileLineCount+" >");
+			System.out.println("NUMBER OF LINE IN A FILE...:  < "+fileLineCount+" >");
 
-		java.io.FileWriter file=new java.io.FileWriter("E:/JDK 11/bin/FileHandle.java");
-		file.append(fileContant);
-		file.flush();
+			java.io.FileWriter file=new java.io.FileWriter("E:/JDK 11/bin/FileHandle.java");
+			file.append(fileContant);
+			file.flush();
+		}
+		catch(java.io.IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
 	}
 	
-	public static void main(String ar[])throws java.io.FileNotFoundException,java.io.IOException
+	public static void main(String ar[])
 	{
 		FileHandle displayContantObj = new FileHandle();
 		String fileContant=displayContantObj.displayFileContant();
